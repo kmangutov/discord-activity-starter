f1a56dad2100f09482e2921eca53a316952285a3
Kirill Mangutov <kmangutov@Kirills-MacBook-Air.local>
2025-04-04 21:20:30 -0500

    try multiplayer

--- client/main.js
+++ client/main.js
@@ -1,18 +1,23 @@
-import { DiscordSDK } from "@discord/embedded-app-sdk";
+import { DiscordSDK, Events } from "@discord/embedded-app-sdk";
 
 import rocketLogo from '/rocket.png';
 import "./style.css";
 
 // Will eventually store the authenticated user's access_token
 let auth;
+// Store participants data
+let participants = [];
 
 const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
 
 setupDiscordSdk().then(() => {
   console.log("Discord SDK is authenticated");
-
-  // We can now make API calls within the scopes we requested in setupDiscordSDK()
-  // Note: the access_token returned is a sensitive secret and should be treated as such
+  
+  // Fetch initial participants
+  fetchParticipants();
+  
+  // Subscribe to participant updates
+  discordSdk.subscribe(Events.ACTIVITY_INSTANCE_PARTICIPANTS_UPDATE, updateParticipants);
 });
 
 async function setupDiscordSdk() {
@@ -57,9 +62,64 @@ async function setupDiscordSdk() {
   }
 }
 
+// Fetch participants in the activity
+async function fetchParticipants() {
+  try {
+    participants = await discordSdk.commands.getInstanceConnectedParticipants();
+    renderParticipants();
+  } catch (error) {
+    console.error("Failed to fetch participants:", error);
+  }
+}
+
+// Update participants when they change
+function updateParticipants(newParticipants) {
+  participants = newParticipants;
+  renderParticipants();
+}
+
+// Render participants in the UI
+function renderParticipants() {
+  const participantsList = document.getElementById('participants-list');
+  if (!participantsList) return;
+  
+  participantsList.innerHTML = '';
+  
+  participants.forEach(user => {
+    // Create avatar URL
+    let avatarSrc = '';
+    if (user.avatar) {
+      avatarSrc = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=256`;
+    } else {
+      const defaultAvatarIndex = Number(BigInt(user.id) >> 22n % 6n);
+      avatarSrc = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarIndex}.png`;
+    }
+    
+    // Create username
+    const username = user.global_name ?? `${user.username}#${user.discriminator}`;
+    
+    // Create participant element
+    const participantElement = document.createElement('div');
+    participantElement.className = 'participant';
+    participantElement.innerHTML = `
+      <img src="${avatarSrc}" alt="Avatar" class="participant-avatar">
+      <span class="participant-name">${username}</span>
+    `;
+    
+    participantsList.appendChild(participantElement);
+  });
+}
+
 document.querySelector('#app').innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
     <h1>Hello, World!</h1>
+    <div class="participants-container">
+      <h2>Activity Participants</h2>
+      <div id="participants-list" class="participants-list"></div>
+    </div>
   </div>
 `;
+
+// Initial render of participants
+renderParticipants();
